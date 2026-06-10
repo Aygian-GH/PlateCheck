3bcc41e2630f947421bbf701e5c738e87ac880c0
Aygian-GH <aaryang9285@gmail.com>
2026-06-10 15:45:48 +0530

    Random Commit

--- Plans & Tools/GitPush.bat
+++ Plans & Tools/GitPush.bat
@@ -1,9 +1,8 @@
 @echo off
-git add .
-$commitName = Read-Host -Prompt "Enter Commit Text: "
-git commit -m "$commitName"
+set /p commitName="Enter Commit Text: "
 
-git remote add origin https://github.com/Aygian-GH/PlateCheck.git
-git branch -M main
-git push -u origin main
+git add .
+git commit -m "%commitName%"
+git push
+
 pause
